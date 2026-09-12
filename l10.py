074d0e47949eb4dee162f4ee3ff8b4b9b3f74099
kouta <kouta114514rise@g-mail.com>
2026-09-12 16:23:59 +0900

    きっと多分おそらくほとんど大体完成

--- l10.py
+++ l10.py
@@ -1,2 +1,22 @@
 import streamlit as st
-st.title("aaaa")
+import time
+
+hours = st.slider("hour", min_value=0, max_value=24,value=0)
+minutes = st.slider("minites", min_value=0, max_value=59,value=0)
+seconds = st.slider("secouns", min_value=0, max_value=59,value=0)
+
+total_seconds = hours * 3600 + minutes * 60 + seconds
+
+if st.button("set up"):
+
+    placeholder = st.empty()
+
+    for t in range(int(total_seconds), -1, -1):
+        h = t // 3600
+        m = (t % 3600) // 60
+        s = t % 60
+
+        placeholder.write(f"地球滅亡まで{h}h{m}m{s}s")
+        time.sleep(1)
+
+    placeholder.write("time up")
